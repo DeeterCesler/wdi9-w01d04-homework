897111549b97c061b8be239346a0f80c2ed64d69
Deeter <deeter.cesler@gmail.com>
2018-09-13 16:59:02 -0600

    Commit 2 - Palindrome

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 // An argument is the actual input a function receives when it's called.
 // 2. return actually delivers what a function needs to do. Console.log is just for the developer.
 // 3. A function needs to return a value for the function to have any real programming value!
+
+// 2.
+
+const checkPalindrome = (str) => {
+    const readyString = str.toLowerCase();
+    const splitString = readyString.split("");
+    splitString.reverse();
+    // console.log(splitString.join(""));
+    if(readyString === splitString.join("")){
+        return true;
+    }
+    else return false;
+}
+
