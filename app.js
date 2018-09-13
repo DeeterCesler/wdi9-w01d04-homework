f061a16987ec174d8b27cf71b146992e8c8372ba
Deeter <deeter.cesler@gmail.com>
2018-09-13 16:29:12 -0600

    Commit 1 - Verbal questions

---
+++ app.js
@@ -0,0 +1,6 @@
+// 1. 
+
+// 1. A parameter is what the uncalled function will receive. 
+// An argument is the actual input a function receives when it's called.
+// 2. return actually delivers what a function needs to do. Console.log is just for the developer.
+// 3. A function needs to return a value for the function to have any real programming value!
